5310eafb5a9fccfd39a5d4e81393e2481bfb94bf
vandersoncurty <vandersoncurty.contato@proton.me>
2024-08-09 15:45:17 -0300

    Código adicionado

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from .routes import main
+    from .routes import main, pokemon
     from .auth import auth
 
     app.register_blueprint(main)
     app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(pokemon)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, jsonify
+import requests
 
-# Cria um blueprint chamado 'main'
 main = Blueprint('main', __name__)
+pokemon = Blueprint('pokemon', __name__)
 
 @main.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@pokemon.route('/pokemon/<name>')
+def get_pokemon(name):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name.lower()}")
+    if response.status_code == 200:
+        pokemon = response.json()
+        data = {
+            "name": pokemon['name'],
+            "id": pokemon['id'],
+            "type": ", ".join([t['type']['name'] for t in pokemon['types']]),
+            "height": pokemon['height'],
+            "weight": pokemon['weight'],
+            "image": pokemon['sprites']['front_default']
+        }
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Pokémon not found!"}), 404
